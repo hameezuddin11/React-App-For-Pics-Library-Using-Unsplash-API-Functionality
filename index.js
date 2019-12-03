5ad05400345b79d44d79dbece7f0adca607bfb1e
Hamzec Uddin <32129686+Hamzec@users.noreply.github.com>
2019-12-04 03:09:23 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+//Import the React DOM and React Librarires
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Components/App";
+
+//Take the react component and show it on screen
+ReactDOM.render(<App />, document.querySelector("#root"));
